f39b467754ba4f18df498ba88299a777be9f9d33
dwk-lzd <2920399114@qq.com>
2025-05-24 16:26:52 +0800

    字符串考点

---
+++ batjtmd/js/str/1.js
@@ -0,0 +1,24 @@
+/* 
+* @func 反转字符串
+* @param {string} str 要反转的字符串
+* @return {string} 反转后的字符串
+*/
+// function reverseString(str) {
+//     // str 是什么类型？ 字符串 简单数据类型 primitive 
+
+//     return str.split('').reverse().join('')
+// }
+
+// 函数表达式
+// es5 函数表达式
+// const reverseString = function (str) {
+//     return str.split('').reverse().join('')
+// }
+
+// es6 箭头函数 简洁 function 不要了 用简单的箭头代替
+// {} 也省略了 只有一句话的时候 
+// 它是返回值的时候 连return 都可以省略
+const reverseString = str => str.split('').reverse().join('')
+console.log(reverseString('hello'));
+
+
---
+++ batjtmd/js/str/2.js
@@ -0,0 +1,8 @@
+"hello".length  // 写法
+// len("hello") 函数式写法是没有的
+// 背后魔法
+// JS 为了面向对象的统一写法
+
+['h', 'e', 'l', 'l', 'o'].reverse()
+
+// hello.length 在底层实现了   "hello" -> new String("hello") 字符串对象 
