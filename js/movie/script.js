2c4a5502d796c72f4465e36b14b477494061f515
dwk-lzd <2920399114@qq.com>
2025-06-09 23:20:31 +0800

    movies item 更改

--- js/movie/script.js
+++ js/movie/script.js
@@ -1,73 +1,70 @@
-// 配置
-// 电影接口地址 
+//配置
+//电影接口地址
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
-// DOM 编程 原生JS
-// 返回的DOM 节点对象
-const oForm = document.querySelector('#form');
-const oInput = document.querySelector('#search');
-console.log(oForm);
-
-// 获取电影 
+//DOM 编程 原生js
+//返回的 DOM 节点对象
+const oForm = document.querySelector('#form')
+const oInput = document.querySelector('#search')
+console.log(oForm)
+//获取电影
 const getMovies = (keyword) => {
     // console.log(keyword)
-    let reqUrl = '';
+    let requestUrl = ''
     if (keyword) {
-        // 搜索
-        reqUrl = SEARCH_API + keyword;
+        //搜索
+        requestUrl = SEARCH_API + keyword
     } else {
-        reqUrl = API_URL;
+        requestUrl = API_URL
     }
-    fetch(reqUrl)
-        // 二进制流
+    fetch(requestUrl)
+        //二进制流
         .then(res => res.json())
         .then(data => {
-            // console.log(data);
-            showMovies(data.results);
+            // console.log(data)
+            showMovies(data.results)
         })
 }
-// movie list render
+//movie list render
 const showMovies = (movies) => {
-    main.innerHTML = '';
+    main.innerHTML = ''
     main.innerHTML = movies.map(movie => {
-        // es6 解构
-        // 将右侧{}解给左侧{} es6 的优雅快捷
-        // 立马称为常量或变量
+        //es6 解构
+        //右边 {} 解给左侧 {} es6 优雅快捷
+        //立马成为常量或变量
         const { poster_path, title, vote_average, overview } = movie
         return `
-    <div class="movie">
-      <img src="${IMG_PATH + movie.poster_path}" alt="${title}">
-      <div class="movie-info">
-        <h3>${title}</h3>
-        <span>${vote_average}</span>
-      </div>
-      <div class="overview">
-        <h3>Overview</h3>
-        ${overview}
-      </div>
-    </div>  
-  `}).join('');
+       <div class = 'movie'>
+        <img src="${IMG_PATH + poster_path}" alt="${title}">     
+        <div class ="movie-info">
+         <h3>${title}</h3>
+         <span${vote_average}</span>
+        </div>
+        <div class="overview">
+         <h3>Overview</h3>
+         ${overview}
+        </div>
+       </div>
+    `}).join('')
 }
-
-// 页面加载完成后执行
+//页面加载完成后执行
 window.onload = function () {
-    getMovies();
+    getMovies()
 }
-
-oForm.addEventListener('submit', function (event) {
-    // 事件对象
-    console.log(event, '////');
-    event.preventDefault();
-    const search = oInput.value.trim();
+oForm.addEventListener('submit', function (e) {
+    //事件对象
+    console.log(e, '////')//event 简写
+    e.preventDefault()
+    const search = oInput.value.trim()
     if (search) {
-        // 搜索电影
+        //搜素电影
         getMovies(search)
     }
-    // console.log(search);
+    // console.log(search)
     // if (search.trim()) {
-    //   console.log(search.trim())
+    //     console.log(search.trim())
     // } else {
-    //   console.log('请输入search')
+    //     console.log('请输入search')
     // }
 })
