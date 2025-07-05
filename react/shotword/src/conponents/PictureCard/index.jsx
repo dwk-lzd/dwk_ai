5918d3343576755be2421714c2088afbae291b44
dwk-lzd <2920399114@qq.com>
2025-07-05 10:23:48 +0800

    shotword TTS

---
+++ react/shotword/src/conponents/PictureCard/index.jsx
@@ -0,0 +1,47 @@
+import './style.css'
+import { useState } from 'react'
+
+
+const PictuerCard = (props) => {
+    const {
+        audio,
+        word,
+        uploadImg
+    } = props
+
+    const [imgPreview, setImgPreview] = useState('https://res.bearbobo.com/resource/upload/W44yyxvl/upload-ih56twxirei.png');
+
+    const uploadImgData = (e) => {
+        const file = (e.target).files?.[0];
+        if (!file) { return; }
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = () => {
+                const data = reader.result;
+                setImgPreview(data);
+                uploadImg(data);
+                resolve(data);
+            }
+            reader.onerror = (error) => { reject(error); };
+        })
+    }
+
+    return (
+        <div className="card">
+            {/* PictureCard */}
+            <input
+                type="file"
+                id="selectImage"
+                accept=".jpg,.jpeg,.png,.gif"
+                onChange={uploadImgData}
+            />
+            <label htmlFor="selectImage" className="upload">
+                <img src={imgPreview} alt="preview" />
+            </label>
+            <div className="word">{word}</div>
+        </div>
+    )
+}
+
+export default PictuerCard
