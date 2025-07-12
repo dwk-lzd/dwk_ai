295032e4edcd99873e3e557e3b65f07038854945
dwk-lzd <2920399114@qq.com>
2025-07-12 23:05:34 +0800

    超链接跳转

--- react/router-demo/src/pages/Home/index.jsx
+++ react/router-demo/src/pages/Home/index.jsx
@@ -2,7 +2,10 @@
 const Home = () => {
     return (
         <>
-            首页
+            首页<br />
+            <a href="http://localhost:5173/about">About</a><br />
+            <a href="http://localhost:5173/user/123">UserProfile123</a><br />
+            <a href="http://localhost:5173/products">Products</a>
         </>
     )
 }
--- react/router-demo/src/pages/Products/index.jsx
+++ react/router-demo/src/pages/Products/index.jsx
@@ -5,6 +5,8 @@ const Products = () => {
         <>
             <h1>产品列表</h1>
             {/* 二级路由的占位符 */}
+            <a href="http://localhost:5173/products/new">NewProduct</a><br />
+            <a href="http://localhost:5173/products/123">ProductDetail123</a><br />
             <Outlet />
         </>
     )
