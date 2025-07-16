9d27923d469fc22b722005f5573443984fc7fd7f
dwk-lzd <2920399114@qq.com>
2025-07-16 12:10:36 +0800

    fragment 文档碎片

---
+++ react/fragment/fragment-demo/src/App.jsx
@@ -0,0 +1,59 @@
+import {
+  useState,
+  Fragment // 文档碎片组件
+} from 'react'
+import './App.css'
+
+// function Demo() {
+//   return (
+
+//     // <h1>Hello</h1>
+//     //   <p>你好</p>
+//     // jsx 最外层一定要有唯一的父元素
+
+//     // 用div的话 DOM 树多了一层不需要的节点，DOM 解析性能下降，多迭代一层
+//     // <Fragment>
+//     //   <h1>Hello</h1>
+//     //   <p>你好</p>
+//     // </Fragment>
+//     <>
+//       <h1>Hello</h1>
+//       <p>你好</p>
+//     </>
+//   )
+// }
+
+function Demo({ items }) {
+  return items.map(item => (
+    <Fragment key={item.id}>
+      <h1>{item.title}</h1>
+      <p>{item.content}</p>
+    </Fragment>
+  ))
+}
+function App() {
+  const items = [
+    {
+      id: 1,
+      title: '标题1',
+      content: '内容1'
+    },
+    {
+      id: 2,
+      title: '标题2',
+      content: '内容2'
+    },
+    {
+      id: 3,
+      title: '标题3',
+      content: '内容3'
+    }
+  ]
+  return (
+    <>
+      <Demo items={items} />
+    </>
+  )
+}
+
+export default App
