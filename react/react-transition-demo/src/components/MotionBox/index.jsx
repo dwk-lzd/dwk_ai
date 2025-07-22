70b527698e33172abdbce28318d1872bfd1dfc66
dwk-lzd <2920399114@qq.com>
2025-07-22 20:34:39 +0800

    framer-motion

--- react/react-transition-demo/src/components/MotionBox/index.jsx
+++ react/react-transition-demo/src/components/MotionBox/index.jsx
@@ -2,14 +2,24 @@ import { motion } from 'framer-motion'
 
 const MotionBox = () => {
     return (
-        <motion.div
-            initial={{ opacity: 0, y: -50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5 }}
-            style={{ background: 'skyblue', padding: 20 }}
-        >
-            <h2>Motion Box</h2>
-        </motion.div>
+        <>
+            <motion.div
+                initial={{ opacity: 0, y: -50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5 }}
+                style={{ background: 'skyblue', padding: 20 }}
+            >
+                <h2>Motion Box</h2>
+            </motion.div>
+            <motion.div
+                initial={{ opacity: 1, y: 0 }}
+                animate={{ opacity: 0, y: -50 }}
+                transition={{ duration: 0.5 }}
+                style={{ background: 'red', padding: 20 }}
+            >
+                <h2>Motion Box2</h2>
+            </motion.div>
+        </>
     )
 }
 
