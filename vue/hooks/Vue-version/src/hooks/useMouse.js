621185cf31e16ded679d4d5ffa85c7a2e25e127e
dwk-lzd <2920399114@qq.com>
2025-08-15 16:29:56 +0800

    初始vue hooks编程理解

---
+++ vue/hooks/Vue-version/src/hooks/useMouse.js
@@ -0,0 +1,30 @@
+import {
+    ref,
+    onMounted,
+    onUnmounted
+} from 'vue'
+
+export function useMouse() {
+    const x = ref(0)
+    const y = ref(0)
+
+    const updateMousePosition = (e) => {
+        x.value = e.clientX
+        y.value = e.clientY
+    }
+    onMounted(() => {
+        // console.log('组件挂载了');
+        window.addEventListener('mousemove', updateMousePosition)
+    })
+
+    onUnmounted(() => {
+        // console.log('组件卸载了');
+        window.removeEventListener('mousemove', updateMousePosition)
+    })
+    return {
+        x,
+        y
+    }
+
+}
+
