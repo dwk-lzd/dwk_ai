fbfcf4f4bdd6f42e714b43aec661a3d0a289b3a9
dwk-lzd <2920399114@qq.com>
2025-09-03 20:15:33 +0800

    数组扁平化

---
+++ interview/js/flatten/2.js
@@ -0,0 +1,8 @@
+// reduce [] => 1项
+// 多维数组   =>  1项一维数组
+// reduce 方法把数组合并成一个值。
+
+const flatten = arr => (
+    arr.reduce((pre, cur) =>
+        pre.concat(Array.isArray(cur) ? flatten(cur) : cur), [])
+)
